b895fd759e28b5a40e608a277779bc081dcf1804
mahmoud makhaly <mahmoud.abdelfatah@5d-agency.com>
2024-09-29 16:54:20 +0300

    -add active class in contact us page

--- js/main.js
+++ js/main.js
@@ -136,15 +136,88 @@ $('.window-carousel').owlCarousel({
         }
     }
 })
-// animation on scroll logic
- AOS.init();
-// scroll animation logic
-var scroll = new SmoothScroll('a[href*="#"]', {
-    speed: 500, // Duration in milliseconds (e.g., 1000ms = 1 second)
-    speedAsDuration: true // Ensures that the speed is treated as duration
-});
-var arrowDown = document.getElementById("scroll-down");
+ 
+ 
+// contact us logic...........
+document.addEventListener("DOMContentLoaded", function () {
+      document.querySelectorAll('.branch-details').forEach(function(link) {
+  link.addEventListener('click', function(event) {
+    event.preventDefault(); // Prevent default action (optional based on link usage)
 
-arrowDown.addEventListener("click", function () {
-  window.scrollTo({ top:700, behavior: "smooth" });
+    // Remove the 'active' class from all .branch-details links
+    document.querySelectorAll('.branch-details').forEach(function(item) {
+      item.classList.remove('active');
+    });
+
+    // Add the 'active' class to the clicked link
+    this.classList.add('active');
+  });
 });
+
+})
+//remove e letter from input in contact us page 
+   document.addEventListener('DOMContentLoaded', function() {
+  const form = document.getElementById('inquiries');
+
+  form.addEventListener('submit', function(event) {
+    event.preventDefault();
+  });
+
+  const phoneInput = document.getElementById('phone');
+
+  phoneInput.addEventListener('keypress', function (e) {
+    const charCode = e.which ? e.which : e.keyCode;
+    if (charCode < 48 || charCode > 57) {
+      e.preventDefault();
+    }
+  });
+  
+  phoneInput.addEventListener('input', function () {
+    this.value = this.value.replace(/[^0-9]/g, '');
+  });
+});
+// map logic in contact us page 
+  var map = L.map('map').setView([51.505, -0.09], 2); 
+  L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+  }).addTo(map);
+
+  var marker;
+  var locationButton = document.getElementById('locationButton'); // Get the button element
+ 
+  var customIcon = L.icon({
+    iconUrl: 'https://unpkg.com/leaflet@1.7.1/dist/images/marker-icon.png',
+    iconSize: [25, 41],
+    iconAnchor: [12, 41],
+    popupAnchor: [1, -34],
+    shadowUrl: 'https://unpkg.com/leaflet@1.7.1/dist/images/marker-shadow.png',
+    shadowSize: [41, 41]
+  });
+
+   function addMarker(lat, lng, location) {
+    if (marker) {
+      map.removeLayer(marker); 
+    }
+    marker = L.marker([lat, lng], { icon: customIcon }).addTo(map);  
+    map.setView([lat, lng], 10); 
+
+  
+    locationButton.onclick = function() {
+       window.open('https://maps.google.com/?q=' + lat + ',' + lng, '_blank');
+    };
+    locationButton.style.display = 'block'; 
+  }
+
+   var links = document.querySelectorAll('.branch-details');
+  links.forEach(function(link) {
+    link.addEventListener('click', function(event) {
+      event.preventDefault();  
+
+       var lat = link.getAttribute('data-lat');
+      var lng = link.getAttribute('data-lng');
+      var location = link.getAttribute('data-location');
+
+ 
+      addMarker(lat, lng, location);
+    });
+  });
